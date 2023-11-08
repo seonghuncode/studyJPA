1f165474102cee4f4e7b9948f515198d5ae61c03
seonghuncode <gnos456@naver.com>
2023-11-08 23:09:11 +0900

    조회, 수정, 삭제

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -7,7 +7,7 @@ import javax.persistence.Persistence;
 
 public class JpaMain {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         //애플리케이션 로딩 시점에 한개만 만든다.
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
@@ -18,25 +18,30 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin(); //트랜잭션 시작
 
-       //실제 동작하는 코드 작성(DB데이터 저장, 불러오기등)
+        //실제 동작하는 코드 작성(DB데이터 저장, 불러오기등)
         Member member = new Member();
 
         try {
-            //JPA에서 모든 변경하는 작업은 트랜잭션 안에서 해야한다.
-            member.setId(1L);
-            member.setName("HelloA");
-            member.setId(2L);
-            member.setName("HelloB");
 
-            em.persist(member); //jpa저장
+            //조회
+            Member findMember = em.find(Member.class, 1L); //자바 컬렉션과 같은 의미(객체를 대신 저장해주는 역할..?)
+            System.out.println("findMember.id : " + findMember.getId());
+            System.out.println("findMember.name : " + findMember.getName());
+
+            //삭제
+//            em.remove(findMember);
+
+            //수정 (jpa통해 가지고온 데이터는 jpa가 관리하기 때문에 jpa가 commit시점에 변경이 있는지 확인하기 대문에 따로 저장을 하지 않아도 된다.)
+            //commit시점에 마지막에 update쿼리를 날려서 업데이트 해준다.
+            findMember.setName("HelloJPA");
 
             tx.commit();
-        }catch(Exception e){
+        } catch (Exception e) {
             tx.rollback();
-        }finally {
+        } finally {
             em.close(); //동작이 끝나면 항상 닫아준다
         }
-        
+
         emf.close();
     }
 
