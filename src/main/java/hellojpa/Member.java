2676880146a1fd94b575f46eb5df70cd72c99da9
seonghuncode <gnos456@naver.com>
2023-11-14 22:02:36 +0900

    필드와 컬럼 매핑

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,46 +1,35 @@
 package hellojpa;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "Member") // 만약 클래스 이름과 테이블 이름이 다르다면 해당 name에 테이블 이름을 작성해 맵핑 가능
 public class Member {
 
-    //PK값을 알려줘야 한다
-    @Id
-    //DB에 있는 컬럼 작성
+    @Id //PK로 매핑
     private Long id;
-    //@Column(name = "name) -> 실제 테이블에 있는 컬럼 이름이랑 변수명이 다를 경우 맵핑 할때 사용
-    private String name;
-    @Column(unique = true, length = 10) //컬럼 설정도 가능
-    private int age;
+    //객체는 username을 사용 DB는 name으로 사용할 경우,  insertable = true, : 컬럼을 수정했을 경우 DB에 insert를 할것인지 말것인지 결정(기본true)
+    @Column(name = "name", insertable = true, updatable = true)
+    private String username;
+    private Integer age;
+    @Enumerated(EnumType.STRING) //자바의 enum타입을 DB에서 사용하고 싶을 경우
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;  //최신버전에서는 private LocalDate testLocalDate; 이런식으로 사용이 가능하다.
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+    @Lob
+    private String description;
 
     public Member(){ //기본생성자가 있어야 문제가 발생하지 않는다.
 
     }
 
     public Member(Long id, String name){
-        this.id = id;
-        this.name = name;
     }
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
 }
