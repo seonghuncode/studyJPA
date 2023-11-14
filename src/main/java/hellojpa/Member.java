71f262fb22c1ba513442a8a7c96b00b37906b9e4
seonghuncode <gnos456@naver.com>
2023-11-14 21:35:17 +0900

    -

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,5 +1,6 @@
 package hellojpa;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -14,6 +15,8 @@ public class Member {
     private Long id;
     //@Column(name = "name) -> 실제 테이블에 있는 컬럼 이름이랑 변수명이 다를 경우 맵핑 할때 사용
     private String name;
+    @Column(unique = true, length = 10) //컬럼 설정도 가능
+    private int age;
 
     public Member(){ //기본생성자가 있어야 문제가 발생하지 않는다.
 
